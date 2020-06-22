0d1e2d1edcd793ef3e40ea3fa73cbe0575e93874
Ercan Ozkaya <ercan@timble.net>
2020-06-22 19:15:48 +0300

    New Release

--- Formula/mason.rb
+++ Formula/mason.rb
@@ -2,14 +2,14 @@ class Mason < Formula
   desc "General masonry"
   homepage "https://github.com/joomlatools/mason"
 
-  version "0.0.8"
+  version "0.1.0"
 
   if OS.mac?
     url "http://files.joomlatools.com/mason@#{version}/mason-macos.tar.gz"
-    sha256 "8d07b982f0b2464ff3512924f2feeb07de31ccc38c00da107903c122e980da77"
+    sha256 "38d904aea27d348aa817b6882ae5d62faa3c73f5e989b560a824d0875f3476b7"
   elsif OS.linux?
     url "http://files.joomlatools.com/mason@#{version}/mason-linux.tar.gz"
-    sha256 "bea491bca707b4dddc87cc876f0aa27ab10b609c6c88005af1144df6b43ea347"
+    sha256 "3b0ddddd6d995f66a26cea2af2a98290a39264fa0faec082a35d83d1e9eb5e40"
   end
 
   def install
